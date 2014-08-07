f902853da38af240d118016bf67803410871ce0e
BeresfordHare <BeresfordHare@users.noreply.github.com>
2014-08-07 14:39:56 +1200

    Source downloaded to new source folder

--- src/3_get_busybox.sh
+++ src/3_get_busybox.sh
@@ -1,13 +1,23 @@
-#/bin/sh
+#!/bin/sh
 
+# Grab everything after the '=' sign
 DOWNLOAD_URL=$(grep -i BUSYBOX_SOURCE_URL .config | cut -f2 -d'=')
+
+# Grab everything after the last '/'
 ARCHIVE_FILE=${DOWNLOAD_URL##*/}
 
-cd work
-rm -f $ARCHIVE_FILE
-wget $DOWNLOAD_URL
-rm -rf busybox
-mkdir busybox
-tar -xvf $ARCHIVE_FILE -C busybox
+cd source
+
+# Downloading busybox source
+# -c option allows the download to resume
+wget -c $DOWNLOAD_URL
+
+# Delete folder with previously extracted busybox
+rm -rf ../work/busybox
+mkdir ../work/busybox
+
+# Extract kernel to folder 'busybox'
+# Full path will be something like, busybox\busybox-1.22.1
+tar -xvf $ARCHIVE_FILE -C ../work/busybox
 cd ..
 

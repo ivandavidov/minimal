0f7779da580b5ace7a61b7e887cb8f986904a483
BeresfordHare <BeresfordHare@users.noreply.github.com>
2014-08-07 14:42:28 +1200

    Added comments

--- src/4_build_busybox.sh
+++ src/4_build_busybox.sh
@@ -1,11 +1,25 @@
-#/bin/sh
+#!/bin/sh
 
 cd work/busybox
-cd $(ls -d *)
-make clean
-make defconfig
-sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
-make busybox
-make install
-cd ../../..
 
+# Change to the first directory ls finds, e.g/ busybox-1.22.1
+cd $(ls -d *)
+
+# Clean's the source?
+make clean
+
+# Create a default configuration file
+make defconfig
+
+# Change the configuration, so that busybox is statically compiled
+# You could do this manually with 'make menuconfig'
+sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
+
+# Compile busybox
+make busybox
+
+# Create the symlinks for busybox
+# It uses the file busybox.links for this
+make install
+
+cd ../../..

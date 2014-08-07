c76f64e0b0a5130bc256f630b1c0ba5937645f58
BeresfordHare <BeresfordHare@users.noreply.github.com>
2014-08-07 14:31:33 +1200

    The downloads now go into a source folder

--- src/1_get_kernel.sh
+++ src/1_get_kernel.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+# Grab everything after the '=' sign
 DOWNLOAD_URL=$(grep -i KERNEL_SOURCE_URL .config | cut -f2 -d'=')
+
+# Grab everything after the last '/'
 ARCHIVE_FILE=${DOWNLOAD_URL##*/}
 
+cd source
+
+# Downloading kernel file
+# -c option allows the download to resume
+wget -c $DOWNLOAD_URL
+
+# Delete folder with previously extracted kernel
+cd ..
 cd work
-rm -f $ARCHIVE_FILE
-wget $DOWNLOAD_URL
 rm -rf kernel
 mkdir kernel
+
+# Extract kernel to folder 'kernel'
+# Full path will be something like, kernel\linux-3.16
 tar -xvf $ARCHIVE_FILE -C kernel
 cd ..
-

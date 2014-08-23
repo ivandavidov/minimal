01956eb5a69daef47af1c3af44264b542fd9b81b
Johannes Altmanninger <j.altmanninger@inbox.lv>
2014-08-23 19:47:06 +0200

    call the scripts with sh so they don't need the executable bit

--- src/build_minimal_linux_live.sh
+++ src/build_minimal_linux_live.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-./0_prepare.sh
-./1_get_kernel.sh
-./2_build_kernel.sh
-./3_get_busybox.sh
-./4_build_busybox.sh
-./5_generate_rootfs.sh
-./6_pack_rootfs.sh
-./7_generate_iso.sh
+sh 0_prepare.sh
+sh 1_get_kernel.sh
+sh 2_build_kernel.sh
+sh 3_get_busybox.sh
+sh 4_build_busybox.sh
+sh 5_generate_rootfs.sh
+sh 6_pack_rootfs.sh
+sh 7_generate_iso.sh

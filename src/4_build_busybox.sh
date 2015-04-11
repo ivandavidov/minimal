b06ab16da9dd69b6b073d2f464ee483b5bfb9de6
Leon Romanovsky <leon@leon.nu>
2015-04-11 18:23:13 +0300

    fix busybox compilation failure on glibc toolchains without RPC support
    Busybox has "RPC networking" enabled in default configuration file.
    In glibc > 2.14 such support was removed by default [1].
    It causes to failures on glibc toolchains without RPC support.
    
    Example of error can be found below:
    ==========
    networking/lib.a(nslookup.o): In function `print_host':
    nslookup.c:(.text.print_host+0x3f): warning: Using 'getaddrinfo' in
    statically linked applications requires at runtime the shared libraries
    from the glibc version used for linking
    debianutils/lib.a(mktemp.o): In function `mktemp_main':
    mktemp.c:(.text.mktemp_main+0x8c): warning: the use of `mktemp' is
    dangerous, better use `mkstemp' or `mkdtemp'
    networking/lib.a(ipcalc.o): In function `ipcalc_main':
    ipcalc.c:(.text.ipcalc_main+0x215): warning: Using 'gethostbyaddr' in
    statically linked applications requires at runtime the shared libraries
    from the glibc version used for linking
    libbb/lib.a(inet_common.o): In function `INET_resolve':
    inet_common.c:(.text.INET_resolve+0x47): warning: Using 'gethostbyname'
    in statically linked applications requires at runtime the shared
    libraries from the glibc version used for linking
    networking/lib.a(inetd.o): In function `reread_config_file':
    inetd.c:(.text.reread_config_file+0x773): warning: Using 'getservbyname'
    in statically linked applications requires at runtime the shared
    libraries from the glibc version used for linking
    networking/lib.a(netstat.o): In function `ip_port_str':
    netstat.c:(.text.ip_port_str+0x3e): warning: Using 'getservbyport' in
    statically linked applications requires at runtime the shared libraries
    from the glibc version used for linking
    networking/lib.a(inetd.o): In function `reread_config_file':
    inetd.c:(.text.reread_config_file+0x6f3): warning: Using 'getrpcbyname'
    in statically linked applications requires at runtime the shared
    libraries from the glibc version used for linking
    networking/lib.a(inetd.o): In function `unregister_rpc':
    inetd.c:(.text.unregister_rpc+0x1f): undefined reference to `pmap_unset'
    networking/lib.a(inetd.o): In function `register_rpc':
    inetd.c:(.text.register_rpc+0x6a): undefined reference to `pmap_unset'
    inetd.c:(.text.register_rpc+0x86): undefined reference to `pmap_set'
    networking/lib.a(inetd.o): In function `prepare_socket_fd':
    inetd.c:(.text.prepare_socket_fd+0x7e): undefined reference to
    `bindresvport'
    collect2: ld returned 1 exit status
    
    [1] http://lists.busybox.net/pipermail/buildroot/2012-June/055173.html
    
    Signed-off-by: Leon Romanovsky <leon@leon.nu>

--- src/4_build_busybox.sh
+++ src/4_build_busybox.sh
@@ -18,6 +18,12 @@ make defconfig
 # Change the configuration, so that busybox is statically compiled
 # You could do this manually with 'make menuconfig'
 sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
+# Busybox has "RPC networking" enabled in default configuration file.
+# In glibc > 2.14 such support was removed by default.
+# http://lists.busybox.net/pipermail/buildroot/2012-June/055173.html
+# It causes to failures on glibc toolchains without RPC support.
+sed -e 's/.*CONFIG_FEATURE_HAVE_RPC.*/CONFIG_FEATURE_HAVE_RPC=n/' -i .config
+sed -e 's/.*CONFIG_FEATURE_INETD_RPC.*/CONFIG_FEATURE_INETD_RPC=n/' -i .config
 
 # Compile busybox
 # TODO - Suggested by Ronny Kalusniok - test this for parallel compilation: "make busybox -j $(grep ^processor /proc/cpuinfo)".

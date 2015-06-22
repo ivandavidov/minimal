c80cbadecf5727e9bb1f2ce654cc76b207d28921
Ronny Kalusniok <r.kalusniok@cosmoproducts.de>
2015-06-22 15:56:35 +0200

    * fix generate_iso

--- src/7_generate_iso.sh
+++ src/7_generate_iso.sh
@@ -5,6 +5,6 @@
 rm -f ${SCRIPTDIR}/${TARGETFILE}
 
 # Generate the ISO image with optimization for "parallel jobs" = "number of processors"
-make isoimage FDINITRD=${SCRIPTDIR}/work/rootfs.cpio.gz -j $(grep -c ^processor /proc/cpuinfo)
+make -C ${SCRIPTDIR}/work/kernel/linux-${KERNEL_VERSION} isoimage FDINITRD=${SCRIPTDIR}/work/rootfs.cpio.gz -j $(grep -c ^processor /proc/cpuinfo)
 
 cp ${SCRIPTDIR}/work/kernel/linux-${KERNEL_VERSION}/arch/x86/boot/image.iso ${SCRIPTDIR}/${TARGETFILE}

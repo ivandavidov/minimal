87ebfa3c39548cb65dcdd437b2122910575c7008
Ronny Kalusniok <r.kalusniok@cosmoproducts.de>
2015-06-22 17:40:06 +0200

    * fix busybox make

--- src/4_build_busybox.sh
+++ src/4_build_busybox.sh
@@ -3,10 +3,10 @@
 . $(dirname $(readlink -f $0 2>/dev/null))/.config
 
 # Remove previously generated artefacts
-make -C ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} clean
+make -C ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} -j $(grep -c ^processor /proc/cpuinfo) clean
 
 # Create a default configuration file
-make ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} defconfig
+make -C ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} -j $(grep -c ^processor /proc/cpuinfo) defconfig
 
 # Change the configuration, so that busybox is statically compiled
 # You could do this manually with 'make menuconfig'
@@ -20,8 +20,8 @@ sed -e 's/.*CONFIG_FEATURE_HAVE_RPC.*/CONFIG_FEATURE_HAVE_RPC=n/' -i .config
 sed -e 's/.*CONFIG_FEATURE_INETD_RPC.*/CONFIG_FEATURE_INETD_RPC=n/' -i .config
 
 # Compile busybox with optimization for "parallel jobs" = "number of processors"
-make busybox -j $(grep -c ^processor /proc/cpuinfo)
+make -C ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} -j $(grep -c ^processor /proc/cpuinfo) busybox 
 
 # Create the symlinks for busybox
 # It uses the file 'busybox.links' for this
-make ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} install
+make ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} -j $(grep -c ^processor /proc/cpuinfo) install

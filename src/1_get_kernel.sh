6cdbe931fbc1766390501cad0ce7f17c6e00c065
Ronny Kalusniok <r.kalusniok@cosmoproducts.de>
2015-06-22 16:34:44 +0200

    * fix filenames for busybox and kernel

--- src/1_get_kernel.sh
+++ src/1_get_kernel.sh
@@ -2,9 +2,6 @@
 
 . $(dirname $(readlink -f $0 2>/dev/null))/.config
 
-# Grab everything after the last '/' character
-ARCHIVE_FILE=${DOWNLOAD_URL##*/}
-
 # Downloading kernel file
 wget -c -N --progress=bar:force --content-disposition --trust-server-names -P ${SCRIPTDIR}/source "${KERNEL_SOURCE_URL}"
 
@@ -14,4 +11,4 @@ rm -rf ${SCRIPTDIR}/work/kernel/linux-*
 
 # Extract kernel to folder 'kernel'
 # Full path will be something like 'work/kernel/linux-3.16.1'
-tar -xvf ${SCRIPTDIR}/source/${ARCHIVE_FILE} -C ${SCRIPTDIR}/work/kernel
+tar -xvf ${SCRIPTDIR}/source/${KERNEL_FILE} -C ${SCRIPTDIR}/work/kernel
--- src/3_get_busybox.sh
+++ src/3_get_busybox.sh
@@ -2,9 +2,6 @@
 
 . $(dirname $(readlink -f $0 2>/dev/null))/.config
 
-# Grab everything after the last '/' character
-ARCHIVE_FILE=${DOWNLOAD_URL##*/}
-
 # Downloading busybox source
 wget -c -N --progress=bar:force --content-disposition --trust-server-names -P ${SCRIPTDIR}/source "${BUSYBOX_SOURCE_URL}"
 
@@ -14,4 +11,4 @@ rm -rf ${SCRIPTDIR}/work/busybox/busybox-*
 
 # Extract busybox to folder 'busybox'
 # Full path will be something like 'work/busybox/busybox-1.23.1'
-tar -xvf ${SCRIPTDIR}/source/${ARCHIVE_FILE} -C ${SCRIPTDIR}/work/kernel
+tar -xvf ${SCRIPTDIR}/source/${BUSYBOX_FILE} -C ${SCRIPTDIR}/work/kernel

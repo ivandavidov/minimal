d19b0062718922373154529040c57d772141f5c1
Ronny Kalusniok <r.kalusniok@cosmoproducts.de>
2015-06-22 17:52:48 +0200

    * merge qemu32 and qemu64

--- src/qemu32.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. $(dirname $(readlink -f $0 2>/dev/null))/.config
-
-qemu-system-i386 -cdrom ${SCRIPTDIR}/${TARGET_FILE}
--- src/qemu64.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. $(dirname $(readlink -f $0 2>/dev/null))/.config
-
-qemu-system-x86_64 -cdrom ${SCRIPTDIR}/${TARGET_FILE}

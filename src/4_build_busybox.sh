7eef3fc54af0ecb8577fa7f4055fb634c29d5cd8
Ronny Kalusniok <r.kalusniok@cosmoproducts.de>
2015-07-02 13:56:39 +0200

    * fix config-path

--- src/4_build_busybox.sh
+++ src/4_build_busybox.sh
@@ -10,14 +10,14 @@ make -C ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} -j $(grep -c ^proce
 
 # Change the configuration, so that busybox is statically compiled
 # You could do this manually with 'make menuconfig'
-sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
+sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" "${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION}/.config"
 
 # Busybox has "RPC networking" enabled in default configuration file.
 # In glibc > 2.14 such support was removed by default.
 # http://lists.busybox.net/pipermail/buildroot/2012-June/055173.html
 # It causes failures on glibc toolchains without RPC support.
-sed -e 's/.*CONFIG_FEATURE_HAVE_RPC.*/CONFIG_FEATURE_HAVE_RPC=n/' -i .config
-sed -e 's/.*CONFIG_FEATURE_INETD_RPC.*/CONFIG_FEATURE_INETD_RPC=n/' -i .config
+sed -i 's/.*CONFIG_FEATURE_HAVE_RPC.*/CONFIG_FEATURE_HAVE_RPC=n/' "${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION}/.config"
+sed -i 's/.*CONFIG_FEATURE_INETD_RPC.*/CONFIG_FEATURE_INETD_RPC=n/' "${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION}/.config"
 
 # Compile busybox with optimization for "parallel jobs" = "number of processors"
 make -C ${SCRIPTDIR}/work/busybox/busybox-${BUSYBOX_VERSION} -j $(grep -c ^processor /proc/cpuinfo) busybox 

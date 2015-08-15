86d24fb37d0d95be1ffd2278f5c43a54d32f638f
Ivan Davidov <davidov.i@gmail.com>
2015-08-15 17:58:05 +0300

    Added more detailed comment descriptions in some of the scripts.

--- src/6_pack_rootfs.sh
+++ src/6_pack_rootfs.sh
@@ -2,10 +2,12 @@
 
 cd work
 
+# Remove the old initramfs archive if it exists.
 rm -f rootfs.cpio.gz
 
 cd rootfs
 
+# Packs the current folder structure in "cpio.gz" archive.
 find . | cpio -H newc -o | gzip > ../rootfs.cpio.gz
 
 cd ../..
--- src/7_generate_iso.sh
+++ src/7_generate_iso.sh
@@ -5,7 +5,9 @@ rm -f minimal_linux_live.iso
 cd work/kernel
 cd $(ls -d *)
 
-# Edit Makefile to look for genisoimage instead of mkisofs
+# Edit Makefile to look for genisoimage instead of mkisofs. This was added as a
+# workaround for some "Debian" and "Arch Linux" distributions. In general this
+# fix should be harmless. 
 sed -i 's/mkisofs/genisoimage/g' arch/x86/boot/Makefile
 
 # Generate the ISO image with optimization for "parallel jobs" = "number of processors"

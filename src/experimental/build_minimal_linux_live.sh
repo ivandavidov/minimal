8302c2a12d30a5d261a237a8c392c0fea7019d5f
Ivan Davidov <davidov.i@gmail.com>
2015-09-13 16:23:52 +0300

    Forgot to update the general build script to include the ToyBox stuff.

--- src/experimental/build_minimal_linux_live.sh
+++ src/experimental/build_minimal_linux_live.sh
@@ -3,8 +3,8 @@
 sh 0_prepare.sh
 sh 1_get_kernel.sh
 sh 2_build_kernel.sh
-sh 3_get_busybox.sh
-sh 4_build_busybox.sh
+sh 3_get_toybox.sh
+sh 4_build_toybox.sh
 sh 5_generate_rootfs.sh
 sh 6_pack_rootfs.sh
 sh 7_generate_iso.sh

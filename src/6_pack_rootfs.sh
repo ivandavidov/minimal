8b96da4dbe445489324efa79ca304f6ccf5a0b45
Ivan Davidov <davidov.i@gmail.com>
2016-01-14 19:33:56 +0200

    John Jolly suggested to use "cpio" with "-R root:root" in order to generate rootfs with all files and folders owned by root (uid 0 and gid 0). This hasn't been tested yet.

--- src/6_pack_rootfs.sh
+++ src/6_pack_rootfs.sh
@@ -7,6 +7,13 @@ rm -f rootfs.cpio.gz
 
 cd rootfs
 
+# Suggested update by John Jolly.
+#
+# find . | cpio -R root:root -H newc -o | gzip > ../rootfs.cpio.gz
+#
+# This produces a root fs with files and directories all owned by user 0, group 0.
+# Note: test this change as soon as possible!
+#
 # Packs the current folder structure in "cpio.gz" archive.
 find . | cpio -H newc -o | gzip > ../rootfs.cpio.gz
 

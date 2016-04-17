cf7e917cb1a48ab51404794bd71f5a0e8d957b5b
Ivan Davidov <davidov.i@gmail.com>
2016-04-17 14:32:44 +0300

    Fexed qemu script to use only the ISO image by default.

--- src/qemu64.sh
+++ src/qemu64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-qemu-system-x86_64 -m 64M -hda hdd.img -cdrom minimal_linux_live.iso -boot d
+qemu-system-x86_64 -m 64M -cdrom minimal_linux_live.iso -boot d
 
 # Use this when you want to play with hard disk content. You can manually create
 # sparse file (/minimal.img) and put overlay content (/minimal.img/rootfs) in it.

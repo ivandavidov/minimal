a510e9ac3e765352c904a976bfa0fd499e9dac14
Ivan Davidov <davidov.i@gmail.com>
2017-03-10 22:31:16 +0200

    Updated software versions. Added experimental 'static-get'. It seems that 'static-get' works fine in normal desktop environment but it doesn't seem to work fine in BusyBox. The problem seems to be somewhere in 'wget' because each downloaded package is ~14KB regardless of its real size.

---
+++ src/minimal_overlay/bundles/static_get/01_get.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+SRC_DIR=$(pwd)
+
+# Find the main source directory
+cd ../../..
+MAIN_SRC_DIR=$(pwd)
+cd $SRC_DIR
+
+# Grab everything after the '=' character.
+DOWNLOAD_URL=$(grep -i STATIC_GET_SOURCE_URL $MAIN_SRC_DIR/.config | cut -f2 -d'=')
+
+# Read the 'USE_LOCAL_SOURCE' property from '.config'
+USE_LOCAL_SOURCE="$(grep -i USE_LOCAL_SOURCE $MAIN_SRC_DIR/.config | cut -f2 -d'=')"
+
+if [ "$USE_LOCAL_SOURCE" = "true" -a ! -f $MAIN_SRC_DIR/source/overlay/static-get.sh  ] ; then
+  echo "Shell script $MAIN_SRC_DIR/source/overlay/static-get.sh is missing and will be downloaded."
+  USE_LOCAL_SOURCE="false"
+fi
+
+cd $MAIN_SRC_DIR/source/overlay
+
+if [ ! "$USE_LOCAL_SOURCE" = "true" ] ; then
+  # Downloading static-get shell script file. The '-c' option allows the download to resume.
+  echo "Downloading static-get shell script from $DOWNLOAD_URL"
+  wget -O static-get.sh -c $DOWNLOAD_URL
+else
+  echo "Using local static-get shell script $MAIN_SRC_DIR/source/overlay/static-get.sh"
+fi
+
+# Delete folder with previously prepared static-get.
+echo "Removing static-get work area. This may take a while..."
+rm -rf ../../work/overlay/staget
+mkdir ../../work/overlay/staget
+
+# Copy static-get to folder 'work/overlay/staget'.
+cp static-get.sh ../../work/overlay/staget
+
+cd $SRC_DIR
+

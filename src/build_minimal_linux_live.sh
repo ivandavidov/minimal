da1bf61bfff1f581d3c833d23aa6a2d33b78df19
jyf <jyf007@gmail.com>
2017-03-19 14:54:01 +0800

    rename as correct name

--- src/build_minimal_linux_live.sh
+++ src/build_minimal_linux_live.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 time bash 00_clean.sh
-time bash 01_get_kernel.sh
+time bash 01_get_kernel.bash
 time bash 02_build_kernel.sh
-time bash 03_get_glibc.sh
+time bash 03_get_glibc.bash
 time bash 04_build_glibc.sh
 time bash 05_prepare_glibc.sh
-time bash 06_get_busybox.sh
+time bash 06_get_busybox.bash
 time bash 07_build_busybox.sh
 time bash 08_prepare_src.sh
 time bash 09_generate_rootfs.sh

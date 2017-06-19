018a94c481ae25cee8a88f0934c1163eb667c980
bauen1 <j2468h@gmail.com>
2017-06-19 17:41:02 +0200

    Fixed nweb

--- src/minimal_overlay/bundles/nweb/bundle.sh
+++ src/minimal_overlay/bundles/nweb/bundle.sh
@@ -9,6 +9,9 @@ cd $SRC_DIR
 # Read the 'CFLAGS' property from '.config'
 CFLAGS="$(grep -i ^CFLAGS $MAIN_SRC_DIR/.config | cut -f2 -d'=')"
 
+echo "removing previous work area"
+rm -rf $MAIN_SRC_DIR/work/overlay/nweb
+mkdir -p $MAIN_SRC_DIR/work/overlay/nweb
 cd $MAIN_SRC_DIR/work/overlay/nweb
 
 # nweb
@@ -24,12 +27,12 @@ install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin"
 install -m755 nweb "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin/nweb"
 #install -m755 client "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin/client"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www" # FHS compliant location
-install -m022 "$SRC_DIR/index.html" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/svc/www/index.html"
-install -m022 "$SRC_DIR/favicon.ico" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/svc/www/favicon.ico"
+install -m644 "$SRC_DIR/index.html" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www/index.html"
+install -m644 "$SRC_DIR/favicon.ico" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www/favicon.ico"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man/man8"
-install -m022 "$SRC_DIR/nweb.8" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man/man8/nweb.8"
+install -m644 "$SRC_DIR/nweb.8" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man/man8/nweb.8"
 
 echo "nweb has been installed."
 echo "type 'man nweb' to see what it can do on your mimial system"

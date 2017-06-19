b7a0182ac3f807d8fcb2875a9691820fdc286d1c
bauen1 <j2468h@gmail.com>
2017-06-19 19:27:54 +0200

    Made the bundle.sh of the nweb overlay better to read

--- src/minimal_overlay/bundles/nweb/bundle.sh
+++ src/minimal_overlay/bundles/nweb/bundle.sh
@@ -22,15 +22,18 @@ cc $CFLAGS $SRC_DIR/nweb23.c -o nweb
 
 echo "nweb has been build."
 
-install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr"
-install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin"
-install -m755 nweb "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin/nweb"
-#install -m755 client "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin/client"
-install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www" # FHS compliant location
-install -m644 "$SRC_DIR/index.html" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www/index.html"
-install -m644 "$SRC_DIR/favicon.ico" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www/favicon.ico"
-install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/etc"
-install -m755 "$SRC_DIR/nweb.sh" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/etc/autorun/90_nweb.sh"
+DESTDIR="$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs"
+
+install -d -m755 "$DESTDIR/usr"
+install -d -m755 "$DESTDIR/usr/bin"
+install -m755 nweb "$DESTDIR/usr/bin/nweb"
+#install -m755 client "$DESTDIR/usr/bin/client"
+install -d -m755 "$DESTDIR/srv/www" # FHS compliant location
+install -m644 "$SRC_DIR/index.html" "$DESTDIR/srv/www/index.html"
+install -m644 "$SRC_DIR/favicon.ico" "$DESTDIR/srv/www/favicon.ico"
+install -d -m755 "$DESTDIR/etc"
+install -d -m755 "$DESTDIR/etc/autorun"
+install -m755 "$SRC_DIR/nweb.sh" "$DESTDIR/etc/autorun/90_nweb.sh"
 
 echo "nweb has been installed."
 echo "it will be autostarted on boot"

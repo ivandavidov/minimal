75f053a9ceebada85c9b90aa4a8b7d7d0a5bd8df
bauen1 <j2468h@gmail.com>
2017-06-19 17:26:42 +0200

    Fixed mixed Makefile and bash syntax in nweb

--- src/minimal_overlay/bundles/nweb/bundle.sh
+++ src/minimal_overlay/bundles/nweb/bundle.sh
@@ -6,13 +6,16 @@ cd ../../..
 MAIN_SRC_DIR=$(pwd)
 cd $SRC_DIR
 
+# Read the 'CFLAGS' property from '.config'
+CFLAGS="$(grep -i ^CFLAGS $MAIN_SRC_DIR/.config | cut -f2 -d'=')"
+
 cd $MAIN_SRC_DIR/work/overlay/nweb
 
 # nweb
-cc -O2 $(SRC_DIR)/nweb23.c -o nweb
+cc $CFLAGS $SRC_DIR/nweb23.c -o nweb
 
 # client
-#cc -O2 $(SRC_DIR)/client.c -o client
+#cc $CFLAGS $SRC_DIR/client.c -o client
 
 echo "nweb has been build."
 
@@ -21,13 +24,12 @@ install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin"
 install -m755 nweb "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin/nweb"
 #install -m755 client "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/bin/client"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/srv/www" # FHS compliant location
-install -m022 "$(SRC_DIR)/index.html" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/svc/www/index.html"
-install -m022 "$(SRC_DIR)/favicon.ico" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/svc/www/favicon.ico"
+install -m022 "$SRC_DIR/index.html" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/svc/www/index.html"
+install -m022 "$SRC_DIR/favicon.ico" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/svc/www/favicon.ico"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man"
 install -d -m755 "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man/man8"
-install -m022 "$(SRC_DIR)/nweb.8" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man/man8/nweb.8"
+install -m022 "$SRC_DIR/nweb.8" "$MAIN_SRC_DIR/work/src/minimal_overlay/rootfs/usr/share/man/man8/nweb.8"
 
 echo "nweb has been installed."
 echo "type 'man nweb' to see what it can do on your mimial system"
-

a70f4bd170eef73a2cddacefef5d24cebdb46ba4
bauen1 <j2468h@gmail.com>
2017-11-14 15:38:23 +0100

    Added dialog bundle

---
+++ src/minimal_overlay/bundles/dialog/02_build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SRC_DIR=$(pwd)
+
+. ../../common.sh
+
+cd $WORK_DIR/overlay/dialog
+
+DESTDIR="$PWD/dialog_installed"
+
+# Change to the dialog source directory which ls finds, e.g. 'dialog-1.3-20170509'.
+cd $(ls -d dialog-*)
+
+echo "Preparing dialog work area. This may take a while..."
+make -j $NUM_JOBS clean
+
+rm -rf $DESTDIR
+
+# Configure dialog
+echo "Configuring dialog..."
+CFLAGS="$CFLAGS" ./configure \
+    --prefix=/usr \
+    --with-ncursesw
+
+echo "Building dialog..."
+make -j $NUM_JOBS
+
+echo "Installing dialog..."
+make -j $NUM_JOBS install DESTDIR=$DESTDIR
+
+echo "Reducing dialog size..."
+strip -g $DESTDIR/usr/bin/*
+
+ROOTFS="$WORK_DIR/src/minimal_overlay/rootfs"
+
+cp -r $DESTDIR/usr/* $ROOTFS
+
+echo "dialog has been installed."
+
+cd $SRC_DIR
+

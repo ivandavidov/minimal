ec6c3a3b81c434296bcf5e1f449467ff2ebdda99
Michael McMahon <michael.steven.mcmahon@gmail.com>
2017-11-16 13:10:02 -0500

    Separated 01_get and 02_build changes

--- src/generate_bundle_skel.sh
+++ src/generate_bundle_skel.sh
@@ -13,12 +13,14 @@ packcaps=${package^^}
 echo "Generating new overlay bundle template for $package..."
 
 cp -r minimal_overlay/bundles/coreutils minimal_overlay/bundles/$package
+
 sed -i "s/.*Full path.*/# Full path will be something like \'work\/overlay\/$package\/$package-$ver\'./" minimal_overlay/bundles/$package/01_get.sh
 sed -i "s/.*Extract coreutils.*/# Extract coreutils to folder \'work\/overlay\/$package\'./" minimal_overlay/bundles/$package/01_get.sh
-sed -i "s/.*source directory which.*/# Change to the coreutils source directory which ls finds, e.g. \'$package-$ver\'./" minimal_overlay/bundles/$package/02_build.sh
 sed -i "s/COREUTILS/$packcaps/g" minimal_overlay/bundles/$package/01_get.sh
-sed -i "s/COREUTILS/$packcaps/g" minimal_overlay/bundles/$package/02_build.sh
 sed -i "s/coreutils/$package/g" minimal_overlay/bundles/$package/01_get.sh
+
+sed -i "s/.*source directory which.*/# Change to the coreutils source directory which ls finds, e.g. \'$package-$ver\'./" minimal_overlay/bundles/$package/02_build.sh
+sed -i "s/COREUTILS/$packcaps/g" minimal_overlay/bundles/$package/02_build.sh
 sed -i "s/coreutils/$package/g" minimal_overlay/bundles/$package/02_build.sh
 
 echo "Created new overlay bundle for $package."

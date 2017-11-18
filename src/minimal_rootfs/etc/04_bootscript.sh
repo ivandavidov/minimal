65738eb238aac245b4d1a77195d0f4e18c94d03d
bauen1 <j2468h@gmail.com>
2017-11-18 18:38:10 +0100

    Fixed 04_bootscript.sh

--- src/minimal_rootfs/etc/04_bootscript.sh
+++ src/minimal_rootfs/etc/04_bootscript.sh
@@ -31,8 +31,9 @@ if [ -d /etc/autorun ] ; then
 for AUTOSCRIPT in /etc/autorun/*
   do
     if [ -f "$AUTOSCRIPT" ] && [ -x "$AUTOSCRIPT" ]; then
-    echo -e "Executing \\e[32m$AUTOSCRIPT\\e[0m in subshell."
-    $AUTOSCRIPT
+      echo -e "Executing \\e[32m$AUTOSCRIPT\\e[0m in subshell."
+      $AUTOSCRIPT
+    fi
   done
 fi
 

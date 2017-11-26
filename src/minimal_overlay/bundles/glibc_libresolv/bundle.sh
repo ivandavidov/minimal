18afd9811df8b03b3400ba9c4be4614ce0b48470
bauen1 <j2468h@gmail.com>
2017-11-26 20:30:58 +0100

    Added glibc_libresolv bundle

---
+++ src/minimal_overlay/bundles/glibc_libresolv/bundle.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+SRC_DIR=$(pwd)
+
+. ../../common.sh
+
+if [ ! -d $SYSROOT ] ; then
+  echo "Cannot continue - GLIBC is missing. Please buld GLIBC first."
+  exit 1
+fi
+
+cd $WORK_DIR/overlay/$BUNDLE_NAME
+
+DESTDIR="$PWD/${BUNDLE_NAME}_installed"
+
+rm -rf $DESTDIR
+
+mkdir -p $DESTDIR/lib
+cp $SYSROOT/lib/libresolv.so.2 $DESTDIR/lib/
+ln -s libresolv.so.2 $DESTDIR/lib/libresolv.so
+
+echo "Reducing $BUNDLE_NAME size"
+strip -g $DESTDIR/lib/*
+
+ROOTFS="$WORK_DIR/src/minimal_overlay/rootfs"
+
+cp -r $DESTDIR/* $ROOTFS
+
+echo "$BUNDLE_NAME has been installed."
+
+cd $SRC_DIR

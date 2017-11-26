545e53200af2d5e798e23bf57d8e35ace12b2939
bauen1 <j2468h@gmail.com>
2017-11-26 21:36:50 +0100

    Removed a hack from the src bundle

--- src/minimal_overlay/bundles/src/bundle.sh
+++ src/minimal_overlay/bundles/src/bundle.sh
@@ -9,11 +9,8 @@ cd $SRC_DIR
 cd $WORK_DIR/src
 
 ROOTFS="$WORK_DIR/src/minimal_overlay/rootfs"
-mkdir -p "$ROOTFS/usr/src"
 
-# maybe use find instead
-shopt -s extglob
-cp -r !("$ROOTFS") "$WORK_DIR/src" "$ROOTFS/usr/src"
-shopt +u extglob
+mkdir -p "$ROOTFS/usr"
+cp -r "$WORK_DIR/src" "$ROOTFS/usr/src"
 
 echo "Source files and folders have been copied to '/src'."

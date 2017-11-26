4ca671a40ff5014f2994a0a79629b4a7ef3e17f3
bauen1 <j2468h@gmail.com>
2017-11-26 22:04:50 +0100

    Modified src bundle to not copy work/src/minimal_overlay/rootfs

--- src/minimal_overlay/bundles/src/bundle.sh
+++ src/minimal_overlay/bundles/src/bundle.sh
@@ -10,7 +10,9 @@ mkdir -p ./minimal_overlay/rootfs/usr/src
 
 shopt -s extglob
 
-cp -r ./!(minimal_overlay/rootfs) ./minimal_overlay/rootfs/usr/src
+cp -r ./!(minimal_overlay) ./minimal_overlay/rootfs/usr/src
+mkdir -p ./minimal_overlay/rootfs/usr/src/minimal_overlay
+cp -r ./minimal_overlay/!(rootfs) ./minimal_overlay/rootfs/usr/src/minimal_overlay/
 
 shopt -u extglob
 

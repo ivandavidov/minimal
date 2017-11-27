37af044fdfdae74bb175436250c38aa57d1bb00b
bauen1 <j2468h@gmail.com>
2017-11-27 21:43:56 +0100

    Updated dhcp dependencies

--- src/minimal_overlay/bundles/dhcp/bundle.sh
+++ src/minimal_overlay/bundles/dhcp/bundle.sh
@@ -20,10 +20,4 @@ install -d -m755 "$DESTDIR/etc/autorun"
 install -m755 "$SRC_DIR/01_network.sh" "$DESTDIR/etc/autorun/01_network.sh"
 install -m755 "$SRC_DIR/05_rc.dhcp" "$DESTDIR/etc/05_rc.dhcp"
 
-# These libraries are necessary for the DNS resolving.
-install -d -m755 "$DESTDIR/lib"
-install -m755 "$SYSROOT/lib/libnss_dns.so.2" "$DESTDIR/lib/libnss_dns.so.2"
-install -m755 "$SYSROOT/lib/libnss_files.so.2" "$DESTDIR/lib/libnss_files.so.2"
-strip -g "$DESTDIR/lib/*" 2>/dev/null
-
 echo "dhcp scripts and libraries have been installed"

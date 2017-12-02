7d2de5053bd2b8881581e0ef114a0ce438a7a87d
bauen1 <j2468h@gmail.com>
2017-12-02 09:52:07 +0100

    Allow the user to enter a custom boot string for syslinux

--- src/13_prepare_iso.sh
+++ src/13_prepare_iso.sh
@@ -47,7 +47,26 @@ cp $WORK_SYSLINUX_DIR/bios/core/isolinux.bin .
 cp $WORK_SYSLINUX_DIR/bios/com32/elflink/ldlinux/ldlinux.c32 .
 
 # Create the ISOLINUX configuration file.
-echo 'default kernel.xz  initrd=rootfs.xz vga=ask' > ./syslinux.cfg
+cat << CEOF > ./syslinux.cfg
+PROMPT 1
+TIMEOUT 50
+DEFAULT mll
+
+SAY Press enter to boot minimal linux or wait 5 seconds
+SAY Press tab to view available boot entries or enter syslinux commands directly
+
+LABEL mll
+	SAY Now booting minimal linux
+        LINUX kernel.xz
+        APPEND vga=ask
+        INITRD rootfs.xz
+
+LABEL mll_nomodeset
+	SAY Now booting minimal linux with 'nomodeset'
+        LINUX kernel.xz
+        APPEND vga=ask nomodeset
+        INITRD rootfs.xz
+CEOF
 
 # Create UEFI start script.
 mkdir -p efi/boot

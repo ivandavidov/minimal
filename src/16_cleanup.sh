41f7110d7ad59922c0cb90e39c3c3ed87962173e
Ivan Davidov <davidov.i@gmail.com>
2017-12-22 01:02:20 +0200

    The original user is not detected properly whn multiple users are logged in. This fix gets the first logged in user.

--- src/16_cleanup.sh
+++ src/16_cleanup.sh
@@ -12,7 +12,7 @@ if [ "$(id -u)" = "0" ] ; then
   echo "Applying original ownership to all affected files. This may take a while."
 
   # Find the original user. Note that this may not always be correct.
-  ORIG_USER=`who | awk '{print \$1}'`
+  ORIG_USER=`who | head -n 1 | awk '{print \$1}'`
   echo "Original user is '$ORIG_USER'."
 
   # Apply ownership back to original owner for all affected files.

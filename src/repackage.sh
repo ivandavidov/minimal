8c11c89915dd465584aa26717e45d9d9dc846012
Ivan Davidov <davidov.i@gmail.com>
2017-12-25 07:59:32 +0200

    Added boot loader download to the 'repackage' script in order to ease the repackaging process when switching between firmware types.

--- src/repackage.sh
+++ src/repackage.sh
@@ -11,6 +11,8 @@ set -e
 ./09_generate_rootfs.sh
 ./10_pack_rootfs.sh
 ./11_generate_overlay.sh
+./12_get_syslinux.sh
+./12_get_systemd-boot.sh
 ./13_prepare_iso.sh
 ./14_generate_iso.sh
 ./15_generate_image.sh

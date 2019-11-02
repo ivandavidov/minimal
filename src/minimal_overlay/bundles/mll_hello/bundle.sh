cc54c7a3f34d912895bb142e05d91cece4e829a6
Ivan Davidov <davidov.i@gmail.com>
2019-11-02 21:52:55 +0200

    All bundles have been updated to their latest versions. Some bundles have been improved.

--- src/minimal_overlay/bundles/mll_hello/bundle.sh
+++ src/minimal_overlay/bundles/mll_hello/bundle.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Break the process if there are any errors
+# Break the shell process if there are any errors
 set -e
 
 # Import common variables and functions
 . ../../common.sh
 
 
-# By convention the overlay bundles have "source" folder, where the source
+# By convention, the overlay bundles have "source" folder, where the source
 # code is downloaded. If the source is downloaded from internet, this allows
 # the download to proceed if the process has been interrupted. In our simple
 # use case we will copy the file "hello.c" and in this way we will simulate
@@ -19,20 +19,20 @@ cp $SRC_DIR/hello.c \
   $OVERLAY_SOURCE_DIR
 
 # The next step is to prepare the source code that we have downloaded in the
-# previous step. Usually this means to extract it. By convention each overlay
+# previous step. Usually this means to extract it. By convention, each overlay
 # bundle has its own root folder where all the build magic happens. In our 
 # simple use case we will "extract" the source code by copying it from the
 # source folder to the main bundle folder.
 #
 # The main overlay folder is "minimal/src/work/overlay" and the bundle overlay
-# folder will be "minimal/src/work/overlay/hello_mll".
+# folder will be "minimal/src/work/overlay/mll_hello".
 mkdir $OVERLAY_WORK_DIR/$BUNDLE_NAME
 cp $OVERLAY_SOURCE_DIR/hello.c \
   $OVERLAY_WORK_DIR/$BUNDLE_NAME
 
 # Each overlay bundle also has a special directory where all build artifacts
 # are put in a structure which represents the final OS folder structure. This
-# folder is "minimal/src/work/overlay/hello_mll/hello_mll_installed".
+# folder is "minimal/src/work/overlay/mll_hello/mll_hello_installed".
 
 # First we create the "destination" folder.
 mkdir $DEST_DIR
@@ -40,6 +40,15 @@ mkdir $DEST_DIR
 # We want our "hello" executable to reside in the "/bin" folder, so we create
 # subfolder "bin" in "$DEST_DIR".
 mkdir $DEST_DIR/bin
+
+# We want our "autorun" script file "90_hello.sh" to reside in "/etc/autorun",
+# so we create the corresponding folders in "$DEST_DIR".
+mkdir -p $DEST_DIR/etc/autorun
+
+# Now we copy the "autorun" script file "90_hello.sh" in "$DEST_DIR/etc/autorun"
+# and we make sure the script is executable.
+cp $SRC_DIR/90_hello.sh $DEST_DIR/etc/autorun
+chmod +x $DEST_DIR/etc/autorun/90_hello.sh
 
 # Now we can compile "$OVERLAY_WORK_DIR/$BUNDLE_NAME/hello.c" and place it in
 # "$DEST_DIR/bin" as executable binary "hello".
@@ -51,11 +60,11 @@ gcc -o $DEST_DIR/bin/hello $OVERLAY_WORK_DIR/$BUNDLE_NAME/hello.c
 reduce_size $DEST_DIR/bin/hello
 
 # No matter what you do with your bundles, no matter how you compile and/or
-# prepare them, in the end all your bundle artifacts must end up in the folder
-# "$OVERLAY_ROOTFS". This special folder represents the final OS structure
-# where all overlay bundles place their final artifacts. In our simple use case
-# we have already prepared appropriate OS folder structure in "$DEST_DIR", so
-# we will simply copy it in "$OVERLAY_ROOTFS".
+# prepare them, in the end all your bundle artifacts must be present in the
+# "$OVERLAY_ROOTFS" folder. This special folder represents the final directory
+# structure where all overlay bundles put their final artifacts. In our simple
+# use case we have already prepared appropriate folder structure in "$DEST_DIR",
+# so we will simply copy it in "$OVERLAY_ROOTFS".
 #
 # The overlay root filesystem folder is "minimal/src/work/overlay_rootfs".
 

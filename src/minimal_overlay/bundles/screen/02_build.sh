fe932ce2b2573b83987efb24ee9e0aff0f5f53a2
Tobias Faller <fallert@tf.uni-freiburg.de>
2021-08-08 16:27:14 +0200

    Added GNU screen overlay bundle

---
+++ src/minimal_overlay/bundles/screen/02_build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+
+. ../../common.sh
+
+cd $WORK_DIR/overlay/$BUNDLE_NAME
+
+# Change to the screen source directory which ls finds, e.g. 'screen-4.8.0'.
+cd $(ls -d screen-*)
+
+if [ -f Makefile ] ; then
+  echo "Preparing '$BUNDLE_NAME' work area. This may take a while."
+  make -j $NUM_JOBS clean
+else
+  echo "The clean phase for '$BUNDLE_NAME' has been skipped."
+fi
+
+rm -rf $DEST_DIR
+
+echo "Configuring '$BUNDLE_NAME'."
+CFLAGS="$CFLAGS" ./configure \
+  --prefix=/usr
+
+echo "Building '$BUNDLE_NAME'."
+make -j $NUM_JOBS
+
+echo "Installing '$BUNDLE_NAME'."
+make -j $NUM_JOBS install DESTDIR=$DEST_DIR
+
+echo "Reducing '$BUNDLE_NAME' size."
+set +e
+strip -g $DEST_DIR/usr/bin/*
+set -e
+
+# With '--remove-destination' all possibly existing soft links in
+# '$OVERLAY_ROOTFS' will be overwritten correctly.
+cp -r --remove-destination $DEST_DIR/* \
+  $OVERLAY_ROOTFS
+
+echo "Bundle '$BUNDLE_NAME' has been installed."
+
+cd $SRC_DIR

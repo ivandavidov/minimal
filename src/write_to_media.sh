dba5faf5693500d8ec4b98977a48e8af8fd9e202
Shivashish <dasshivashish11@gmail.com>
2022-01-03 13:59:12 +0530

    Added warnings in write_to_media.sh

--- src/write_to_media.sh
+++ src/write_to_media.sh
@@ -14,10 +14,11 @@ if ! [ -e $ISO_NAME ]; then
 	exit 1
 
 elif [ "$#" -ne 1 ]; then
-	echo "Invocation: $0 [DEVICE_NAME] "
-	echo "Example: $0 /dev/sda "
+	echo "Usage: $0 [DEVICE_NAME] (eg. $0 /dev/sda)"
 	exit 1
 else 
+	echo "CAUTION: All data on device $1 will be erased"
+	echo "You have been warned"
 	sudo dd if=$ISO_NAME of=$1 bs=4M && sync
 fi
 

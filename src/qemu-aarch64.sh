4ba3f497cf01280fe812ce0f732caa87a25108b2
Ivan Davidov <davidov.i@gmail.com>
2022-06-29 20:03:45 +0000

    Added all ARM64 (AArch64) build dependencies. Optimized the RAM setting for QEMU.

--- src/qemu-aarch64.sh
+++ src/qemu-aarch64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt -smp 2 -m 4096 \
+qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine type=virt -smp 2 -m 256 \
   -kernel work/kernel/kernel_installed/kernel \
   -initrd work/rootfs.cpio.xz \
   -append "vga=ask"
